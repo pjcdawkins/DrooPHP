7696140a9b50ac32b5f595188274298e08043744
Patrick Dawkins <pjcdawkins@gmail.com>
2012-03-18 23:55:24 +0000

    Improved parsing so it now tails the file for metadata, much better scalability.

--- DrooPHP/Election.php
+++ DrooPHP/Election.php
@@ -28,27 +28,18 @@ class DrooPHP_Election {
   public $options = array();
 
   /**
-   * The character position of the start of the first ballot line in the file.
-   *
-   * @var int
-   */
-  protected $_ballot_first_line_start;
-
-  /**
-   * The character position of the end of the last ballot line in the file.
-   *
-   * @var int
-   */
-  protected $_ballot_last_line_end;
-
-  /**
    * Constructor: initiate a count by loading a BLT file.
    *
    * @todo allow passing in a complete profile instead of a file, perhaps.
    */
   public function __construct($filename, $options = array()) {
     $this->loadOptions($options);
-    $this->file = fopen($filename, 'r');
+    if (file_exists($filename) && is_readable($filename)) {
+      $this->file = fopen($filename, 'r');
+    }
+    else {
+      throw new DrooPHP_Exception('File does not exist or cannot be read: ' . $filename);
+    }
     $this->profile = new DrooPHP_Election_Profile;
     $this->parse();
   }
@@ -74,77 +65,124 @@ class DrooPHP_Election {
 
   /**
    * Parse the BLT file to create the election profile.
-   *
-   * @todo get around performance vs format limitations (requiring new lines and not supporting multiline comments)
-   *
-   * @todo work out how to 'tail' the file to get metadata lines instead of reading everything
    */
   public function parse() {
-    $file = $this->file;
-    $profile = $this->profile;
     try {
-      $i = 0;
-      while (($line = fgets($file)) !== FALSE) {
-        // Remove comments (starting with # or // until the end of the line).
-        $line = preg_replace('/(\x23|\/\/).*/', '', $line);
-        // Skip blank lines.
-        $untrimmed = $line;
-        $line = trim($line);
-        if (!strlen($line)) {
-          continue;
-        }
-        $i++;
-        if ($i === 1) {
-          // First line should always be "num_candidates num_seats".
-          $parts = explode(' ', $line);
-          if (count($parts) != 2) {
-            throw new DrooPHP_Exception('The first line must contain exactly two parts.');
-          }
-          $num_candidates = (int) $parts[0];
-          $profile->setNumCandidates($num_candidates);
-          $profile->setNumSeats($parts[1]);
-          continue;
-        }
-        else if ($i === 2 && strpos($line, '-') === 0) {
-          // If line 2 starts with a minus sign, it specifies the withdrawn candidate IDs.
-          $withdrawn = explode(' -', substr($line, 1));
-          $withdrawn = array_map('intval', $withdrawn); // Candidate IDs are always integers.
-          $profile->setWithdrawn($withdrawn);
-          continue;
-        }
-        else if ($line === '0') {
-          // A line containing just 0 will be the one straight after the final ballot line.
-          $this->_ballot_last_line_end = ftell($file) - strlen($untrimmed);
-        }
-        else if (strpos($line, '"') === 0) {
-          // A line beginning with " is a metadata line (not a ballot line).
-          if (!isset($metadata_first_line)) {
-            $metadata_first_line = $i;
-          }
-          // The first n metadata lines are the candidate names, where n is the number of candidates.
-          // The other metadata lines are the election title, then (if Wichmann-style) a source and then a comment line.
-          if ($i - $metadata_first_line < $num_candidates) {
-            $profile->addCandidate(trim($line, '"'));
-          }
-          else if ($profile->title === NULL) {
-            $profile->title = trim($line, '"');
-          }
-          else if ($profile->source === NULL) {
-            $profile->source = trim($line, '"');
-          }
-          else if ($profile->comment === NULL) {
-            $profile->comment = trim($line, '"');
-          }
-          continue;
-        }
-        else if ($this->_ballot_first_line_start === NULL) {
-          // All other lines are ballot lines. We only need to know about the first one for now.
-          $this->_ballot_first_line_start = ftell($file) - strlen($untrimmed);
-        }
-      }
+      $this->_parseHead();
+      $this->_parseTail();
+      return TRUE;
     }
     catch (Exception $e) {
-      throw new DrooPHP_Exception('Error in BLT data line ' . $i . ': ' . $e->getMessage());
+      throw new DrooPHP_Exception(
+        'Error in BLT data, line ' . ftell($file) . ': ' . $e->getMessage()
+      );
+      return FALSE;
+    }
+  }
+
+  /**
+   * Read information from the beginning (head) of the BLT file.
+   *
+   * @return void
+   */
+  protected function _parseHead() {
+    $file = $this->file;
+    $profile = $this->profile;
+    rewind($file);
+    $i = 0;
+    while (($line = fgets($file)) !== FALSE && $i <= 2) {
+      // Remove comments (starting with # or // until the end of the line).
+      $line = preg_replace('/(\x23|\/\/).*/', '', $line);
+      // Trim whitespace.
+      $line = trim($line);
+      // Skip blank lines.
+      if (!strlen($line)) {
+        continue;
+      }
+      $i++;
+      if ($i === 1) {
+        // First line should always be "num_candidates num_seats".
+        $parts = explode(' ', $line);
+        if (count($parts) != 2) {
+          throw new DrooPHP_Exception('The first line must contain exactly two parts.');
+        }
+        $profile->setNumCandidates($parts[0]);
+        $profile->setNumSeats($parts[1]);
+      }
+      else if ($i === 2 && strpos($line, '-') === 0) {
+        // If line 2 starts with a minus sign, it specifies the withdrawn candidate IDs.
+        $withdrawn = explode(' -', substr($line, 1));
+        $withdrawn = array_map('intval', $withdrawn); // Candidate IDs are always integers.
+        $profile->setWithdrawn($withdrawn);
+      }
+    }
+  }
+
+  /**
+   * Read information from the end (tail) of the BLT file.
+   *
+   * @return void
+   */
+  protected function _parseTail() {
+    $file = $this->file;
+    $profile = $this->profile;
+    $num_candidates = $profile->getNumCandidates();
+    /*
+     There can be a maximum of $num_candidates + 3 tail lines (each candidate
+     is named and then there are optionally election, title, and source).
+    */
+    $lines_to_read = $num_candidates + 3;
+    // Read the tail of the file.
+    $pos = -2;
+    $tail = array();
+    // Keep seeking backwards through the file until the number of lines left to read is 0.
+    while ($lines_to_read > 0) {
+      $char = NULL;
+      // Keep seeking backwards through the line until finding a line feed character.
+      while ($char !== "\n" && fseek($file, $pos, SEEK_END) !== -1) {
+        $char = fgetc($file);
+        $pos--;
+      }
+      $line = fgets($file);
+      // Remove comments (starting with # or // until the end of the line).
+      $line = preg_replace('/(\x23|\/\/).*/', '', $line);
+      // Trim whitespace.
+      $line = trim($line);
+      // Skip blank lines.
+      if (!strlen($line)) {
+        continue;
+      }
+      /*
+       A line containing just 0 marks the end of the ballot lines, so stop
+       before reading anything before it (remember we're reading backwards).
+      */
+      if ($line === '0') {
+        break;
+      }
+      $tail[] = $line;
+      $lines_to_read--;
+    }
+    // Reverse so we can read forwards (because optional lines are at the end).
+    $tail = array_reverse($tail);
+    // The minimum number of lines is the number of candidates.
+    if (count($tail) < $num_candidates) {
+      throw new DrooPHP_Exception('Candidate names not found');
+    }
+    foreach ($tail as $key => $line) {
+      $info = trim($line, '"');
+      if ($key < $num_candidates) {
+        // This line is a candidate.
+        $profile->addCandidate($info);
+      }
+      else if ($profile->title === NULL) {
+        $profile->title = trim($line, '"');
+      }
+      else if ($profile->source === NULL) {
+        $profile->source = trim($line, '"');
+      }
+      else if ($profile->comment === NULL) {
+        $profile->comment = trim($line, '"');
+      }
     }
   }
 
