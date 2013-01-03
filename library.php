229b6552eb53261552eb4a166ed0bd8e3f08057a
Patrick Dawkins <pjcdawkins@gmail.com>
2013-01-03 00:24:39 +0000

    Fail (with Exception) in library.php if Composer has not been run.

--- library.php
+++ library.php
@@ -1,10 +1,15 @@
 <?php
-ini_set('display_errors', TRUE);
-ini_set('display_startup_errors', TRUE);
-
 /**
- * @file
- * Include this file to load the DrooPHP library.
+ * @file Include this file to load the DrooPHP library.
+ *
+ * @package DrooPHP
+ * @author Patrick Dawkins <pjcdawkins@gmail.com>
  */
+
+// The library requires Composer (see getcomposer.org). Composer creates an
+// autoload file; check that it exists.
+if (!is_readable(__DIR__ . '/vendor/autoload.php')) {
+  throw new Exception('Autoload file not available (have you run Composer?)');
+}
 
 require 'vendor/autoload.php';
