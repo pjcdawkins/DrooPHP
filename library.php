69b8c2d8dc489c4be02c371527239eec73e42525
Patrick Dawkins <pjcdawkins@gmail.com>
2014-05-14 21:00:54 +0100

    Move CLI bin file; allow for global installation.

--- library.php
+++ library.php
@@ -5,8 +5,10 @@
 
 // The library requires Composer (see getcomposer.org). Composer creates an
 // autoload file; check that it exists.
-if (!is_readable(__DIR__ . '/vendor/autoload.php')) {
+$local_autoload = __DIR__ . '/vendor/autoload.php';
+$global_autoload = __DIR__ . '/../../../vendor/autoload.php';
+if (!is_readable($local_autoload) && !is_readable($global_autoload)) {
   throw new Exception('Autoload file not available (have you run Composer?)');
 }
 
-require 'vendor/autoload.php';
+require is_readable($local_autoload) ? $local_autoload : $global_autoload;
